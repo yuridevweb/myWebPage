d02371e42c2de82f97d0e5971bed65ae855eb843
yuridevweb <yuridevweb@gmail.com>
2022-01-10 12:51:10 +0000

    modal projects gallery added

--- main/static/main/scripts.js
+++ main/static/main/scripts.js
@@ -1,24 +1,40 @@
 // Modal element
 let modal = document.getElementById('ProjectInfo');
-//Modal open btn
-let modalBtn = document.getElementsByClassName('modalBtn');
-//Modal close btn
-let closeBtn = document.getElementById('closeBtn');
-//Listen for outside click
-window.addEventListener('click', outsideClick);
-
-modalBtn.addEventListener('click', modalOpen);
-closeBtn.addEventListener('click', modalClose);
-
 function modalOpen() {
   modal.style.display = 'block';
 }
 function modalClose() {
   modal.style.display = 'none';
 }
+//Listen for outside click
+window.addEventListener('click', outsideClick);
 
 function outsideClick(e) {
   if (e.target == modal) {
     modal.style.display = 'none';
   }
+}
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+
 }
